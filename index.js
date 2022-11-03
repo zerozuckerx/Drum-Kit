975ae4d8dc62dd68256663f83ff28b04e850359c
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-11-03 14:58:59 +0100

    added keydown listener

--- index.js
+++ index.js
@@ -5,44 +5,56 @@ for(let i = 0; i < buttonArray.length; i++) {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-        let tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
+    makeSound(buttonInnerHTML);
 
-      case "a":
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "s":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "d":
-        let kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      case "j":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "k":
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "l":
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      default:
-        console.log(buttonInnerHTML);
-    }
   });
+}
+
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+
+})
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "a":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "s":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "j":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "k":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "l":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
 }
